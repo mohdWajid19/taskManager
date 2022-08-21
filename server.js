9ecc0f71e152eafeb1ab2ba69bfcdc199126a915
mohdWajid <mohdwajeed1919@gmail.com>
2022-08-21 18:59:50 +0530

    added feature to display tasks

--- server.js
+++ server.js
@@ -6,9 +6,74 @@ const app = new express()
 dotenv.config();
 
 app.set('view engine', 'ejs')
-
+const data = [
+    {
+        'taskName' : 'eat',
+        'completed' : false
+    },
+    {
+        'taskName' : 'sleep',
+        'completed' : false
+    },
+    {
+        'taskName' : 'jog',
+        'completed' : false
+    },
+    {
+        'taskName' : 'run',
+        'completed' : false
+    },
+    {
+        'taskName' : 'eat',
+        'completed' : false
+    },
+    {
+        'taskName' : 'sleep',
+        'completed' : false
+    },
+    {
+        'taskName' : 'jog',
+        'completed' : false
+    },
+    {
+        'taskName' : 'run',
+        'completed' : true
+    },
+    {
+        'taskName' : 'eat',
+        'completed' : true
+    },
+    {
+        'taskName' : 'sleep',
+        'completed' : true
+    },
+    {
+        'taskName' : 'jog',
+        'completed' : true
+    },
+    {
+        'taskName' : 'run',
+        'completed' : true
+    },
+    {
+        'taskName' : 'eat',
+        'completed' : true
+    },
+    {
+        'taskName' : 'sleep',
+        'completed' : true
+    },
+    {
+        'taskName' : 'jog',
+        'completed' : true
+    },
+    {
+        'taskName' : 'run',
+        'completed' : true
+    }    
+]
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {data: data})
 })
 
 
